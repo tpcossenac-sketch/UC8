093124f1a88574201e5e35ed5ad9924b89927a24
tpcossenac-sketch <tpcossenac@gmail.com>
2026-08-13 09:13:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const btnGravador = document.getElementById("btn-gravador");
+const statusGravacao = document.getElementById("status-gravacao");
+
+// Quando o usuário pressionar o botão
+btnGravador.addEventListener("pointerdown", () => {
+    btnGravador.style.backgroundColor = "#e74c3c";
+    btnGravador.textContent = "🔴 Gravando... Não solte!";
+
+    statusGravacao.textContent = "Status: Capturando áudio...";
+});
+
+// Quando o usuário soltar o botão
+btnGravador.addEventListener("pointerup", () => {
+    btnGravador.style.backgroundColor = "#3498db";
+    btnGravador.textContent = "🎤 Clique e Segure para Gravar";
+
+    statusGravacao.textContent = "Status: Gravação concluída e enviada!";
+});
+
+        // Caso o toque seja cancelado (ex: chamada no celular)
+btnGravador.addEventListener('pointercancel', () => {
+  resetarBotao();
+});
+
