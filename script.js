d18af8d2b632a7535e16e37ec1331ddcdea11753
tpcossenac-sketch <tpcossenac@gmail.com>
2026-08-26 08:37:13 -0300

    arrumando o js

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
-const btnGravador = document.getElementById("btn-gravador");
-const statusGravacao = document.getElementById("status-gravacao");
+const painelLog = document.getElementById('log-painel');
 
-// Quando o usuário pressionar o botão
-btnGravador.addEventListener("pointerdown", () => {
-    btnGravador.style.backgroundColor = "#e74c3c";
-    btnGravador.textContent = "🔴 Gravando... Não solte!";
+function registrarLog(mensagem) {
 
-    statusGravacao.textContent = "Status: Capturando áudio...";
+    const novaLinha = document.createElement('div');
+    novaLinha.textContent = `[${new Date().toLocaleTimeString()}] ${mensagem}`;
+    painelLog.appendChild(novaLinha);
+}
+
+// 1. EVENTOS DE MOUSE
+const btnClique = document.getElementById('btn-clique');
+const areaMouse = document.getElementById('area-mouse');
+
+btnClique.addEventListener('click', function() {
+    registrarLog("[Click] Evento de mouse: Evento 'click' disparado");
+
 });
 
-// Quando o usuário soltar o botão
-btnGravador.addEventListener("pointerup", () => {
-    btnGravador.style.backgroundColor = "#3498db";
-    btnGravador.textContent = "🎤 Clique e Segure para Gravar";
-
-    statusGravacao.textContent = "Status: Gravação concluída e enviada!";
+// 1.1 EVENTOS DE MOUSE: mouseenter
+areaMouse.addEventListener('mouseenter', function() {
+    areaMouse.textContent = 'Mouse entrou na área!';
+    areaMouse.style.backgroundColor = '#0b810b';
+    registrarLog("[Mouse entrou] Evento de mouse: Evento 'mouseenter' disparado");
 });
 
-        // Caso o toque seja cancelado (ex: chamada no celular)
-btnGravador.addEventListener('pointercancel', () => {
-  resetarBotao();
+// 1.2 EVENTOS DE MOUSE: mouseleave
+areaMouse.addEventListener('mouseleave', function() {
+    areaMouse.textContent = 'Mouse saiu da área!';
+    areaMouse.style.backgroundColor = '#e74c3c';
+    registrarLog("[Mouse saiu] Evento de mouse: Evento 'mouseleave' disparado");
 });
 
+
+// 2. EVENTOS DE TECLADO
+const campoTeclado = document.getElementById('campo-teclado');
+
+campoTeclado.addEventListener('keydown', function(event) {
+    registrarLog(`[Tecla Pressionada] Evento de teclado: Evento 'keydown' disparado`);
+});
+
+// 2.1 EVENTOS DE TECLADO: keyup                                             
+campoTeclado.addEventListener('keyup', function(event) {
+    registrarLog(`[Tecla Liberada] Evento de teclado: Evento 'keyup' disparado`);
+});
+
+// 3. EVENTOS DE FORMULÁRIO
+const meuFormulario = document.getElementById('meu-formulario');
+const campoInput = document.getElementById('campo-input');
+
+campoInput.addEventListener('input', function(event) {
+    registrarLog(`[Input] Evento de formulário: Evento 'input' disparado -> Valor atual: ${event.target.value}`);
+});
+
+// // 3.1 EVENTOS DE FORMULÁRIO: submit
+
+// meuFormulario.addEventListener('submit', function(event) {
+//     event.preventDefault(); // Evita o envio do formulário paeara fins de demonstração
+//     registrarLog(`[Submit] Evento de formulário: Evento 'submit' disparado -> Valor do campo: ${campoInput.value}`);
+// });
+
+
+// 3.2 EVENTOS DE FORMULÁRIO: submit (acesso ao evento de envio via id do formulário)
+const campoEnvio = document.getElementById('campo-envio');
+
+campoEnvio.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o envio do formulário paeara fins de demonstração
+    registrarLog(`[Submit] Evento de formulário: Evento 'submit' disparado`);
+});
